d08fe6fb58c90872e27589b5c15072e5a29ecd79
Pabitra Kumar Bebartta <pabitrakumarbebartta@gmail.com>
2023-04-06 19:53:24 +0530

    mini bug

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import "./FooterStyles.css"
-
 import { FaHome, FaPhone, FaMailBulk, FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa"
 import { Link } from 'react-router-dom'
 
